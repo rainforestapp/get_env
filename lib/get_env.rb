fe35e1feb42a5a925cc861f706ee628da65907f1
Paul Padier <paul.padier@rainforestapp.com>
2023-10-19 11:52:18 +0900

    Nest methods in metaclass definition
    This makes it easier to mark the new methods as private.

--- lib/get_env.rb
+++ lib/get_env.rb
@@ -3,44 +3,48 @@
 require 'get_env/version'
 
 module GetEnv
-  def self.[](key)
-    return nil if key.nil?
+  class << self
+    def [](key)
+      return nil if key.nil?
 
-    v = ENV[key].to_i
-    return v if v.to_s == ENV[key]
+      v = ENV[key].to_i
+      return v if v.to_s == ENV[key]
 
-    v = ENV[key].to_f
-    return v if v.to_s == ENV[key]
+      v = ENV[key].to_f
+      return v if v.to_s == ENV[key]
 
-    return false if ENV[key] == 'false'
-    return true if ENV[key] == 'true'
+      return false if ENV[key] == 'false'
+      return true if ENV[key] == 'true'
 
-    ENV[key]
-  end
-
-  def self.fetch(*args, &block)
-    case args
-    in [key, default] then fetch_with_default(key, default)
-    in [key] then fetch_without_default(key, &block)
-    else raise ArgumentError
+      ENV[key]
     end
-  end
 
-  def self.fetch_with_default(key, default)
-    if ENV.has_key?(key)
-      self[key]
-    else
-      default
+    def fetch(*args, &block)
+      case args
+      in [key, default] then fetch_with_default(key, default)
+      in [key] then fetch_without_default(key, &block)
+      else raise ArgumentError
+      end
     end
-  end
 
-  def self.fetch_without_default(key, &block)
-    if ENV.has_key?(key)
-      self[key]
-    elsif block_given?
-      block.call
-    else
-      ENV.fetch(key) # Will raise a KeyError
+    private
+
+    def fetch_with_default(key, default)
+      if ENV.has_key?(key)
+        self[key]
+      else
+        default
+      end
+    end
+
+    def fetch_without_default(key, &block)
+      if ENV.has_key?(key)
+        self[key]
+      elsif block_given?
+        block.call
+      else
+        ENV.fetch(key) # Will raise a KeyError
+      end
     end
   end
 end
